d6b9566882bb877f38f4a6229ea00569ef92bea1
55amira <93343085+55amira@users.noreply.github.com>
2021-10-31 01:38:50 +0200

    add files
    i added two classes to repository

---
+++ sheet1.m
@@ -0,0 +1,58 @@
+
+A=[-7 5 -9 ; 2 -1 2; 1 -1 -2]
+
+B=[16 3 2 13 ; 5 10 11 8 ; 9 6 7 12 ; 4 15 14 1]
+
+C=[4 2 -3 ; 7 -7 9 ; 3 -5 6]
+
+D=[6 3 2 ; 2 12 -7 ;-1 6 2 ; -5 15 11]
+
+%% Q1
+
+x= 3*A-5*C
+
+% 7*A+2*B is error becouse of number of rows and couloms in matrix A !=  number of rows and couloms in matrix B
+
+y = C*A
+
+z = C*D'
+
+%% Q2 
+
+v1=zeros(5)
+
+v2=zeros(2,3)
+
+x1=ones(4)
+
+x2=ones(3,2)
+
+y1=size(4,4)
+
+y2=zeros(size(4,4))
+
+z1=diag([1 2 3 4])
+
+z2=eye(3)
+
+%% Q3
+% [A,B] is error becouse of number of rows in matrix A !=  number of rows in matrix B
+
+%[A;B] is error becouse of number of coulom in matrix A !=  number of coulom in matrix B
+
+
+%% Q4
+
+
+x=diag([ 5 5 5 5 5 5 5 ]);
+x(:,8)=5
+
+%%Q5
+x=A(2,:)
+x1=A(:,3)
+
+
+
+
+ 
+ 
